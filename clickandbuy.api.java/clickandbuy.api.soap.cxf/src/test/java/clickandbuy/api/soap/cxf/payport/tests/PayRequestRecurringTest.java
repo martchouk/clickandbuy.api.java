51b66319e8452fde0473dc5b9f453d45a869fdca
nicolae petridean <nicolae.petridean@gmail.com>
2013-03-06 23:48:23 +0200

    cleanning

--- clickandbuy.api.java/clickandbuy.api.soap.cxf/src/test/java/clickandbuy/api/soap/cxf/payport/tests/PayRequestRecurringTest.java
+++ clickandbuy.api.java/clickandbuy.api.soap.cxf/src/test/java/clickandbuy/api/soap/cxf/payport/tests/PayRequestRecurringTest.java
@@ -32,11 +32,7 @@ import com.clickandbuy.api.soap.cxf.PayRequestRequest;
 import com.clickandbuy.api.soap.cxf.PayRequestResponse;
 import com.clickandbuy.api.soap.cxf.RecurringPaymentAuthorization;
 import com.clickandbuy.api.soap.cxf.StatusRequestDetails;
-import com.clickandbuy.api.soap.cxf.StatusRequestRequest;
-import com.clickandbuy.api.soap.cxf.StatusRequestResponse;
 import com.clickandbuy.api.soap.cxf.TransactionIDList;
-import com.clickandbuy.api.soap.cxf.TransactionIDStatus;
-import com.clickandbuy.api.soap.cxf.TransactionStatus;
 
 /**
  * Tests related to PayRequestRecurring
@@ -58,36 +54,29 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 	@Before
 	public void setUp() throws Exception {
 		configureCertificatesPolicy();
-
 		recurringPaymentAuthorizationID = doPayRequest();
 	}
 
 	/**
 	 * Test the PayRequestRecurring
-	 * @throws InterruptedException 
+	 * 
+	 * @throws InterruptedException
 	 */
 	@Test
 	public void testPayRequestRecurring() throws InterruptedException {
-		
-//		TransactionStatus status  = null;
-//		
-//		while (status != TransactionStatus.CANCELLED){
-//			status = doStatusRequest();
-//			logger.debug("Current status: " + status);
-//			Thread.sleep(3000);
-//		}
+
 		logger.debug("Preparing to make a PayRequestRecurring");
 
 		PayRequestRecurringResponse payRequestRecurringResponse = new PayRequestRecurringResponse();
 
-		PayRequestRecurringRequest payRequestRecurringRequest = new PayRequestRecurringRequest();
+		final PayRequestRecurringRequest payRequestRecurringRequest = new PayRequestRecurringRequest();
 		payRequestRecurringRequest.setAuthentication(prepareAuthenticationBasedOnProjectID());
 		payRequestRecurringRequest.setDetails(preparePayRequestRecurringDetails());
 
 		try {
 			payRequestRecurringResponse = payPortType.payRequestRecurring(payRequestRecurringRequest);
 			logger.debug("Created recurring payment with Id: " + payRequestRecurringResponse.getTransaction().getTransactionID());
-		} catch (ErrorDetails_Exception errorDetails_Exception) {
+		} catch (final ErrorDetails_Exception errorDetails_Exception) {
 			logger.error(errorDetails_Exception.getFaultInfo().getDescription());
 		}
 	}
@@ -96,7 +85,7 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 		Long recurringAuthorizationID = null;
 		PayRequestResponse payRequestResponse = null;
 
-		PayRequestRequest payRequestRequest = new PayRequestRequest();
+		final PayRequestRequest payRequestRequest = new PayRequestRequest();
 		payRequestRequest.setAuthentication(prepareAuthenticationBasedOnProjectID());
 		payRequestRequest.setDetails(preparePayRequestDetails());
 
@@ -105,42 +94,17 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 			logger.debug("transaction: " + payRequestResponse.getTransaction());
 			logger.debug("created reccuring payment authorization: " + payRequestResponse.getTransaction().getCreatedRecurringPaymentAuthorization());
 			logger.debug("created reccuring payment authorization status: " + payRequestResponse.getTransaction().getCreatedRecurringPaymentAuthorization().getRecurringPaymentAuthorizationStatus());
-			
+
 			recurringAuthorizationID = payRequestResponse.getTransaction().getCreatedRecurringPaymentAuthorization().getRecurringPaymentAuthorizationID();
 			transactionID = payRequestResponse.getTransaction().getTransactionID();
-			
+
 			logger.debug("Created transaction with Id: " + payRequestResponse.getTransaction().getTransactionID());
-		} catch (ErrorDetails_Exception errorDetails_Exception) {
+		} catch (final ErrorDetails_Exception errorDetails_Exception) {
 			logger.error(errorDetails_Exception.getFaultInfo().getDescription());
 		}
 
 		return recurringAuthorizationID;
 	}
-	
-	private TransactionStatus doStatusRequest() {
-		TransactionStatus transactionStatus = null;
-		
-		StatusRequestResponse statusRequestResponse = null;
-
-		StatusRequestRequest statusRequestRequest = new StatusRequestRequest();
-		statusRequestRequest.setAuthentication(prepareAuthenticationBasedOnProjectID());
-		statusRequestRequest.setDetails(prepareStatusRequestDetails());
-
-		try {
-			statusRequestResponse = payPortType.statusRequest(statusRequestRequest);
-
-			for (TransactionIDStatus transactionIDStatus : statusRequestResponse.getTransactionList().getTransaction()) {
-				transactionStatus = transactionIDStatus.getTransactionStatus();
-				logger.debug("The transaction with ID: " + transactionID + " have the status: " + transactionStatus);
-			}
-
-		} catch (ErrorDetails_Exception errorDetails_Exception) {
-			logger.error(errorDetails_Exception.getFaultInfo().getDescription());
-		}
-		
-		return transactionStatus;
-	}
-	
 
 	/**
 	 * Prepares an StatusRequestDetails. As stated in the WSDL, at this point we can add one of the following:
@@ -154,7 +118,7 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 	 * @return
 	 */
 	public StatusRequestDetails prepareStatusRequestDetails() {
-		StatusRequestDetails statusRequestDetails = new StatusRequestDetails();
+		final StatusRequestDetails statusRequestDetails = new StatusRequestDetails();
 
 		statusRequestDetails.setTransactionIDList(prepareTransactionIDList());
 
@@ -162,7 +126,7 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 	}
 
 	private TransactionIDList prepareTransactionIDList() {
-		TransactionIDList transactionIDList = new TransactionIDList();
+		final TransactionIDList transactionIDList = new TransactionIDList();
 
 		transactionIDList.getTransactionID().add(transactionID);
 
@@ -175,36 +139,19 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 	 * 
 	 * @param payRequestFormBean
 	 * @return T_PayRequest_Details
-	 * @throws DatatypeConfigurationException 
-	 * @throws ParseException 
+	 * @throws DatatypeConfigurationException
+	 * @throws ParseException
 	 * @throws PayServiceException
 	 */
 	private PayRequestDetails preparePayRequestDetails() throws ParseException, DatatypeConfigurationException {
 		// preparing the externalID
 		externalId = externalId + System.nanoTime() + "_";
 		logger.debug("***externalId:" + externalId);
-		
-		PayRequestDetails payRequestDetails = new PayRequestDetails();
 
-		// TODO fill in necessary test data
-
-		// Billing Address
-		// LegalEntity billing = prepareLegalEntity(payRequestFormBean.getBillingAddress(),
-		// payRequestFormBean.isUseBillingAddress());
-		// payRequestDetails.setBilling(billing);
-
-		// RecurringAuthorization
-		// RecurringPaymentAuthorization createRecurring = prepareRecurringAuthorization(payRequestFormBean.getCreateRecurring(),
-		// payRequestFormBean.isUseRecurringAuthorization());
-		// payRequestDetails.setCreateRecurring(createRecurring);
-
-		// Shipping Address
-		// LegalEntity shipping = prepareLegalEntity(payRequestFormBean.getShippingAddress(),
-		// payRequestFormBean.isUseShippingAddress());
-		// payRequestDetails.setShipping(shipping);
+		final PayRequestDetails payRequestDetails = new PayRequestDetails();
 
 		// Order Details
-		OrderDetails order = new OrderDetails();
+		final OrderDetails order = new OrderDetails();
 		order.setItemList(new OrderDetailItemList());
 		order.setText(payPortTestDataSupplier.getPayRequestText());
 
@@ -225,15 +172,11 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 
 		payRequestDetails.setCreateRecurring(prepareRecurringPaymentAuthorization());
 
-		// payRequestDetails.setBilling(value);
-		// payRequestDetails.setCreateRecurring(value);
-		// payRequestDetails.setShipping(value);
-
 		return payRequestDetails;
 	}
 
 	private RecurringPaymentAuthorization prepareRecurringPaymentAuthorization() throws ParseException, DatatypeConfigurationException {
-		RecurringPaymentAuthorization recurringPaymentAuthorization = new RecurringPaymentAuthorization();
+		final RecurringPaymentAuthorization recurringPaymentAuthorization = new RecurringPaymentAuthorization();
 
 		recurringPaymentAuthorization.setDescription(payPortTestDataSupplier.getPayRequestCreateRecurringDescription());
 		recurringPaymentAuthorization.setExpireDate(TestUtil.toXMLGregorianCalendar(payPortTestDataSupplier.getPayRequestCreateRecurringExpireDate()));
@@ -244,7 +187,7 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 	}
 
 	private Money prepareAmount() {
-		Money money = new Money();
+		final Money money = new Money();
 
 		money.setAmount(payPortTestDataSupplier.getPayRequestRecurringAmount());
 		money.setCurrency(payPortTestDataSupplier.getPayRequestRecurringCurrency());
@@ -256,7 +199,7 @@ public class PayRequestRecurringTest extends PayPortParentTest {
 	 * @return
 	 */
 	private PayRequestRecurringDetails preparePayRequestRecurringDetails() {
-		PayRequestRecurringDetails payRequestRecurringDetails = new PayRequestRecurringDetails();
+		final PayRequestRecurringDetails payRequestRecurringDetails = new PayRequestRecurringDetails();
 
 		// preparing the externalID
 		externalId = externalId + System.nanoTime() + "_";
