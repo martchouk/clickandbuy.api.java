1a1146717d38faab0f22f7e8c4d9ef104ba652d6
Ciprian I. Ileana <ciprian.ileana@trimplement.com>
2013-03-12 12:23:14 +0200

    Removed AXIS2 module of the project.

--- clickandbuy.api.java/clickandbuy.api.soap.axis2/README.txt
+++
@@ -1,2 +0,0 @@
-# To generate the the soap client stub run:
-mvn axis2-wsdl2code:wsdl2code
--- clickandbuy.api.java/clickandbuy.api.soap.axis2/src/main/java/com/clickandbuy/api/soap/axis2/stub/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.clickandbuy.com/webservices/pay_1_1_0/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.clickandbuy.api.soap.axis2.stub;
